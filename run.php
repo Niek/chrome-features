116b5a5bb1b33753afcb42ab8a622b213c8ad68b
Niek van der Maas <mail@niekvandermaas.nl>
2021-01-20 18:55:02 +0100

    Add blink settings + search

--- run.php
+++ run.php
@@ -6,7 +6,8 @@ $Parsedown = new Parsedown();
 
 $xml = simplexml_load_file('xml/namespacefeatures.xml');
 $lines = simplexml_load_file('xml/content__features_8cc.xml')->compounddef->programlisting->codeline;
-$json = json5_decode(file_get_contents('./runtime_enabled_features.json5'), true);
+$blinkFeatures = json5_decode(file_get_contents('./runtime_enabled_features.json5'), true);
+$blinkSettings = json5_decode(file_get_contents('./settings.json5'), true);
 
 $features = [];
 
@@ -56,11 +57,14 @@ foreach ($xml->compounddef->sectiondef as $i) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Chrome features</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/list.js/2.3.1/list.min.js"></script>
 </head>
 
 <body>
   <section class="section">
     <div class="container">
+      <input class="input is-large" type="text" placeholder="Type to search settings..." id="search">
+      <hr>
       <h1 class="title">
         Chrome features
       </h1>
@@ -75,13 +79,11 @@ foreach ($xml->compounddef->sectiondef as $i) {
             <th>Enabled by default</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody class="list">
           <?php
-
           foreach ($features as $f) {
-            echo '<tr><td>' . $f['name'] . '</td><td>' . (count($f['description']) > 0 ? '<pre>' . $Parsedown->line(implode(PHP_EOL, $f['description'])) . '</pre>' : '&mdash;') . '</td><td>' . ($f['enabled_default'] ? '✅' : '❌') . '</td></tr>';
+            echo '<tr><td class="name">' . $f['name'] . '</td><td>' . (count($f['description']) > 0 ? '<pre>' . $Parsedown->line(implode(PHP_EOL, $f['description'])) . '</pre>' : '&mdash;') . '</td><td>' . ($f['enabled_default'] ? '✅' : '❌') . '</td></tr>';
           }
-
           ?>
         </tbody>
       </table>
@@ -101,18 +103,52 @@ foreach ($xml->compounddef->sectiondef as $i) {
             <th>Enabled by default</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody class="list">
           <?php
-
-          foreach ($json['data'] as $f) {
-            echo '<tr><td>' . $f['name'] . '</td><td>&mdash;</td><td>' . (isset($f['status']) && $f['status'] === 'stable' ? '✅' : '❌') . '</td></tr>';
+          foreach ($blinkFeatures['data'] as $f) {
+            echo '<tr><td class="name">' . $f['name'] . '</td><td>&mdash;</td><td>' . (isset($f['status']) && $f['status'] === 'stable' ? '✅' : '❌') . '</td></tr>';
           }
-
+          ?>
+        </tbody>
+      </table>
+    </div>
+    <div class="container">
+      <h1 class="title">
+        Blink settings
+      </h1>
+      <p class="subtitle">
+        Modify with <span class="is-family-monospace">--blink-settings</span>:
+      </p>
+      <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Default</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody class="list">
+          <?php
+          foreach ($blinkSettings['data'] as $f) {
+            echo '<tr><td class="name">' . $f['name'] . '</td><td>' . (isset($f['initial']) ? preg_replace('/^\'|\'$/', '', var_export($f['initial'], true)) : '&mdash;') . '</td><td>' . ($f['type'] ?? '&mdash;') . '</td></tr>';
+          }
           ?>
         </tbody>
       </table>
     </div>
   </section>
+  <script>
+    var lists = [];
+    document.querySelectorAll('table').forEach(t => {
+      lists.push(new List(t, { valueNames: ['name'] }))
+    });
+
+    document.querySelector('#search').addEventListener('keyup', k => {
+      lists.forEach(e => {
+        e.search(document.querySelector('#search').value);
+      });
+    });
+  </script>
 </body>
 
 </html>
