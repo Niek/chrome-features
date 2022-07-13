4ceab1ff1c6e3705f8a59e7fe6f48695fc5c038d
Niek van der Maas <mail@niekvandermaas.nl>
2022-07-13 14:28:05 +0200

    Switch to branch `main`

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-wget -q "https://raw.githubusercontent.com/chromium/chromium/master/content/public/common/content_features.cc" -O content_features.cc
-wget -q "https://raw.githubusercontent.com/chromium/chromium/master/chrome/common/pref_names.cc" -O pref_names.cc
-wget -q "https://raw.githubusercontent.com/chromium/chromium/master/third_party/blink/renderer/platform/runtime_enabled_features.json5" -O runtime_enabled_features.json5
-wget -q "https://raw.githubusercontent.com/chromium/chromium/master/third_party/blink/renderer/core/frame/settings.json5" -O settings.json5
+wget -q "https://raw.githubusercontent.com/chromium/chromium/main/content/public/common/content_features.cc" -O content_features.cc
+wget -q "https://raw.githubusercontent.com/chromium/chromium/main/chrome/common/pref_names.cc" -O pref_names.cc
+wget -q "https://raw.githubusercontent.com/chromium/chromium/main/third_party/blink/renderer/platform/runtime_enabled_features.json5" -O runtime_enabled_features.json5
+wget -q "https://raw.githubusercontent.com/chromium/chromium/main/third_party/blink/renderer/core/frame/settings.json5" -O settings.json5
 
 rm -rf xml
 echo -e "GENERATE_HTML=NO\nGENERATE_LATEX=NO\nGENERATE_XML=YES\nQUIET=YES\nFILE_PATTERNS=*.cc" | doxygen -
